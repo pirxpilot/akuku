b99dd3039d3e5787ed26510c6a40e83fd34315df
Damian Krzeminski <pirxpilot@furkot.com>
2019-03-07 09:31:56 -0700

    replace mocha with tape

--- test/akuku.js
+++ test/akuku.js
@@ -1,51 +1,48 @@
-var assert = require('assert');
-var akuku = require('../lib/akuku');
+const test = require('tape');
+const akuku = require('../lib/akuku');
 
-describe('akuku', function () {
-  it('measure time and count', function (done) {
-    let tm = akuku('test ABC');
+test('measure time and count', function (t) {
+  let tm = akuku('test ABC');
 
-    tm.start();
+  t.plan(7);
+  tm.start();
 
-    assert.ok(tm.toObject().activeNow, 'should be active after start');
+  t.ok(tm.toObject().activeNow, 'should be active after start');
+
+  setTimeout(function() {
+
+    tm.stop();
+
+    let { counter } = tm.toObject();
+    t.equal(counter, 1);
+
+    tm.start(undefined, 3);
 
     setTimeout(function() {
       tm.stop();
 
-      let { counter } = tm.toObject();
-      assert.equal(counter, 1);
+      let { activeNow, active, counter } = tm.toObject();
 
-      tm.start(undefined, 3);
+      t.ok(!activeNow);
+      t.equal(active[0], 0, 'not even a second elapsed');
+      t.ok(active[1] > 50 * 1e6, 'some nanos must have elapsed');
+      t.equal(counter, 4);
+      t.ok(/\s+4 test ABC\s+0 hours 0 minutes 0 seconds/.test(tm.toString()));
 
-      setTimeout(function() {
-        tm.stop();
+    }, 37);
+  }, 25);
 
+});
 
-        let { activeNow, active, counter } = tm.toObject();
-        assert.ok(!activeNow);
-        assert.equal(active[0], 0, 'not even a second elapsed');
-        assert(active[1] > 50 * 1e6, 'some nanos must have elapsed');
-        assert.equal(counter, 4);
+test('should wrap next', function(t) {
+  t.plan(1);
 
-        assert.ok(/\s+4 test ABC\s+0 hours 0 minutes 0 seconds/.test(tm.toString()));
+  let tm = akuku('test BCD');
 
-        done();
-      }, 37);
-    }, 25);
+  function next() {
+    t.ok(true);
+  }
 
-  });
-
-  it('should wrap next', function(done) {
-    let tm = akuku('test BCD');
-
-    function next() {
-      done();
-    }
-
-    let fn = tm.start(next);
-    setTimeout(fn, 22);
-
-  });
-
-
+  let fn = tm.start(next);
+  setTimeout(fn, 22);
 });
--- test/counters.js
+++ test/counters.js
@@ -1,21 +1,21 @@
-var assert = require('assert');
+const test = require('tape');
 var counters = require('../lib/counters');
 
-describe('counter', function () {
-  it('must count', function () {
-    let c = counters();
-    let countA = c.add('A');
-    let countB = c.add('B');
+test('must count', function (t) {
+  let c = counters();
+  let countA = c.add('A');
+  let countB = c.add('B');
 
-    countA();
+  countA();
 
-    assert.deepEqual(c.toObject(), { A: 1, B: 0 });
-    assert.ok(/1 A\s+0 B/.test(c.toString()));
+  t.deepEqual(c.toObject(), { A: 1, B: 0 });
+  t.ok(/1 A\s+0 B/.test(c.toString()));
 
-    countB();
-    countA(3);
+  countB();
+  countA(3);
 
-    assert.deepEqual(c.toObject(), { A: 4, B: 1 });
-    assert.ok(/4 A\s+1 B/.test(c.toString()));
-  });
+  t.deepEqual(c.toObject(), { A: 4, B: 1 });
+  t.ok(/4 A\s+1 B/.test(c.toString()));
+
+  t.end();
 });
